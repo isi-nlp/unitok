7be0f960a67ea08945a7e5a78d97664926bfe393
Jon May <jonmay@isi.edu>
2016-02-05 10:23:07 -0800

    offsets in vanilla unitok

--- scripts/tok.py
+++ scripts/tok.py
@@ -7,7 +7,7 @@ from collections import defaultdict as dd
 import re
 import os.path
 import gzip
-import unicodedata as ud
+import unicodedata as ud # pip install unicodedata
 scriptdir = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -15,6 +15,22 @@ scriptdir = os.path.dirname(os.path.abspath(__file__))
 excluded = set([ud.lookup("TIBETAN MARK INTERSYLLABIC TSHEG"), # tshegs appear between syllables
                 ud.lookup("TIBETAN MARK DELIMITER TSHEG BSTAR"),
                 ])
+
+reader = codecs.getreader('utf8')
+writer = codecs.getwriter('utf8')
+
+# TODO: make this py3 safe (as a nop)
+def prepfile(fh, code):
+  ret = gzip.open(fh.name, code) if fh.name.endswith(".gz") else fh
+  if code.startswith('r'):
+    ret = reader(fh)
+  elif code.startswith('w'):
+    ret = writer(fh)
+  else:
+    sys.stderr.write("I didn't understand code "+code+"\n")
+    sys.exit(1)
+  return ret
+
 
 def tokenizeoffset(data):
   ''' given a token without whitespace, return offsets for where split should happen;
@@ -30,22 +46,54 @@ def tokenizeoffset(data):
   return sorted(list(ret))
 
 # TODO: replace tokenize with splitoninst(tokenizeoffset) per original word? to avoid code duplication
+# but note offset stuff
 def tokenize(data):
   toks = []
-  for char in data:
+  offsets = []
+  lastStart = None
+  lastNW = None
+  for offset, char in enumerate(data):
     cc = ud.category(char)
+#    print char, offset, cc
+    isWhitespace=False
+    if cc.startswith("Z") or cc=="Cc":
+      isWhitespace=True
+#      print "Whitespace"
+      if lastStart is not None:
+#        print "Closing last region %d-%d" % (lastStart, lastNW)
+        offsets.append("%d-%d" % (lastStart, lastNW))
+        lastStart = None
+        lastNW = None
     if (cc.startswith("P") or cc.startswith("S")) and char not in excluded:
+#      print "Tokenizing"
       toks.append(' ')
       toks.append(char)
       toks.append(' ')
+      if lastStart is not None:
+#        print "Closing last region %d-%d" % (lastStart, lastNW)
+        offsets.append("%d-%d" % (lastStart, lastNW))
+        lastStart = None
+        lastNW = None
+#      print "Isolating %d-%d" % (offset, offset)
+      offsets.append("%d-%d" % (offset, offset))
     else:
+#      print "Appending"
       toks.append(char)
-  return ' '.join(''.join(toks).split())
+      if not isWhitespace:
+        if lastStart is None:
+#          print "Starting new token %d" % offset
+          lastStart = offset
+        lastNW = offset
+  if lastStart is not None:
+#    print "Finishing up and closing last region %d-%d" % (lastStart, lastNW)
+    offsets.append("%d-%d" % (lastStart, lastNW))
+  return ' '.join(''.join(toks).split()), offsets
 
 def main():
   parser = argparse.ArgumentParser(description="unicode-based tokenization",
                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
   parser.add_argument("--infile", "-i", nargs='?', type=argparse.FileType('rb'), default=sys.stdin, help="input file")
+  parser.add_argument("--offsetsfile", "-f", nargs='?', type=argparse.FileType('w'), default=None, help="file to write tokenization offsets (0-based space-seg start-end relative to original). default is not to write these")
   parser.add_argument("--outfile", "-o", nargs='?', type=argparse.FileType('w'), default=sys.stdout, help="output file")
 
 
@@ -54,17 +102,18 @@ def main():
     args = parser.parse_args()
   except IOError, msg:
     parser.error(str(msg))
-
-  reader = codecs.getreader('utf8')
-  writer = codecs.getwriter('utf8')
-  infile = gzip.open(args.infile.name, 'r') if args.infile.name.endswith(".gz") else args.infile
-  infile = reader(infile)
-  outfile = gzip.open(args.outfile.name, 'w') if args.outfile.name.endswith(".gz") else args.outfile
-  outfile = writer(outfile)
-
+  infile = prepfile(args.infile, 'r')
+  outfile = prepfile(args.outfile, 'w')
+  offsetsfile = None
+  if args.offsetsfile is not None:
+    offsetsfile = prepfile(args.offsetsfile, 'w')
+  
 
   for line in infile:
-    outfile.write(tokenize(line)+"\n")
+    tokenization, offsets = tokenize(line)
+    outfile.write(tokenization+"\n")
+    if offsetsfile is not None:
+      offsetsfile.write(' '.join(offsets)+"\n")
 
 if __name__ == '__main__':
   main()
